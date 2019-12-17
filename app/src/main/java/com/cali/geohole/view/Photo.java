e301ace44a270ae865921c52323793e0bf8dec3a
Lino Alfonso <lmena@kubo.co>
2019-12-17 14:19:34 -0500

    image

--- app/src/main/java/com/cali/geohole/view/Photo.java
+++ app/src/main/java/com/cali/geohole/view/Photo.java
@@ -1,15 +1,31 @@
 package com.cali.geohole.view;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.cali.geohole.R;
 
+import java.io.File;
+import java.io.IOException;
+
+import id.zelory.compressor.Compressor;
+import pl.aprilapps.easyphotopicker.DefaultCallback;
+import pl.aprilapps.easyphotopicker.EasyImage;
+
 public class Photo extends Fragment {
+
+    private ImageView image;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,58 @@ public class Photo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photo, container, false);
+        View view = inflater.inflate(R.layout.fragment_photo, container, false);
+        EasyImage.configuration(getContext()).setImagesFolderName("Images") // images folder name, default is "EasyImage"
+                .saveInAppExternalFilesDir() // if you want to use root internal memory for storying images
+                .saveInRootPicturesDirectory();
+        image = view.findViewById(R.id.imageViewLogF);
+        return view;
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        EasyImage.handleActivityResult(requestCode, resultCode, data, getActivity(), new DefaultCallback() {
+            @Override
+            public void onImagePickerError(Exception e, EasyImage.ImageSource source, int type) {
+                //Some error handling
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onImagePicked(File imageFile, EasyImage.ImageSource source, int type) {
+                try {
+                    File compressedFile = compressImage(getContext(),imageFile);
+                    Uri imageUri= Uri.fromFile(compressedFile);
+                    image.setImageURI(imageUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+            }
+
+            @Override
+            public void onCanceled(EasyImage.ImageSource source, int type) {
+                //Cancel handling, you might wanna remove taken photo if it was canceled
+                if (source == EasyImage.ImageSource.CAMERA) {
+                    File photoFile = EasyImage.lastlyTakenButCanceledPhoto(getContext());
+                    if (photoFile != null) photoFile.delete();
+                }
+            }
+        });
+
+    }
+
+    public static File compressImage(Context context, File fileToReduce) throws IOException {
+        File compressedImage = new Compressor(context)
+                .setMaxWidth(640)
+                .setMaxHeight(480)
+                .setQuality(75)
+                .setCompressFormat(Bitmap.CompressFormat.PNG)
+                .setDestinationDirectoryPath(Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES).getAbsolutePath()).compressToFile(fileToReduce);
+
+        return compressedImage;
     }
 }
